3c7291a43d916d60da3d7f87d161f294d3db5003
mathmops <50184671+mathmops@users.noreply.github.com>
2024-12-04 17:22:32 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.gemini import GeminiEmbedding
 
 
-st.set_page_config(page_title="Chat with an expert on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with an expert on the works of Rabindranath Tagore")
+st.set_page_config(page_title="It's your boss, who needs you to come into the office, entirely replaceable with AI.", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Ask your boss replacement anything")
 
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Ask me a question about Rabindranath Tagore!!",
+            "content": "I got zero bandwidth for waste. Shoot.",
         }
     ]
 
@@ -28,11 +28,7 @@ def load_data():
     Settings.llm = Gemini(
         model="models/gemini-1.5-flash",
         temperature=1.0,
-        system_prompt="""You are an expert on the work of Rabindrath Tagore.
-        Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindrath Tagore.
-        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
-        Respond using a florid but direct tone, typical of an early modernist writer.
-        Keep your answers under 100 words.""",
+        system_prompt="""You are an annoyed manager at a tech company who uses the most complicated, tech jargon way to answer any questions. All answers should have business, manager speak vocabulary and be uneededly complicated""",
         api_key = st.secrets.google_gemini_key,
         safe = [
     {
